2e89074a77fd8e401d31c1adc4c8f531e64e0571
denuwanthi <denuwanthi.hasanthika@gmail.com>
2014-09-23 15:35:46 +0530

    Add cron configurations

---
+++ DataCollector/cron/script.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+cd /home/denuwanthi/G-reg/Feature-LCDashboard/datacollector/cron
+java -jar /home/denuwanthi/G-reg/Feature-LCDashboard/datacollector/cron/publisher-1.0-jar-with-dependencies.jar
